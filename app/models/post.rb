a945d5864890ca4cedc146f415c01f17f452c759
taku38550 <taku_s.1028@icloud.com>
2023-02-08 15:56:27 +0900

    UserとPostのモデルとテーブル作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :text,  presence: true
+  validates :image, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,         presence: true
+  validates :password,         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :family_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
+  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
+  validates :cultivated_crops, presence: true
+       
+  has_many :posts
+  
 end
