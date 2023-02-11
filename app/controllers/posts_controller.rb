89c9c1d7103484ac2ee939ea62b95496a1edead9
taku38550 <taku_s.1028@icloud.com>
2023-02-11 15:37:34 +0900

    編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,9 +22,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   private
