9f7a4ed11a35fa4f671cf663f501fab7cbc27d55
taku38550 <taku_s.1028@icloud.com>
2023-02-11 13:41:20 +0900

    詳細表示ページの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,9 @@ class PostsController < ApplicationController
 
   end
 
+  def show
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :text, :image).merge(user_id: current_user.id)
