92c8e042d996a0e94a7eb85a158bcc70048e020a
taku38550 <taku_s.1028@icloud.com>
2023-02-13 10:08:42 +0900

    Userのコントローラー、アクション、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: :create
   end
