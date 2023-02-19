4706c5dae51d19b4a3655c6893e7d5523f4326e2
taku38550 <taku_s.1028@icloud.com>
2023-02-19 14:56:12 +0900

    messagesテーブルの作成

---
+++ app/models/messgae.rb
@@ -0,0 +1,2 @@
+class Messgae < ApplicationRecord
+end
---
+++ db/migrate/20230219055408_create_messgaes.rb
@@ -0,0 +1,10 @@
+class CreateMessgaes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :messgaes do |t|
+      t.string :content
+      t.references :room, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
