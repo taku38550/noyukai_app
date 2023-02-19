2edd9a8aa95a1988c4420a80ff7ff51718c5f92a
taku38550 <taku_s.1028@icloud.com>
2023-02-19 14:53:50 +0900

    messagesコントローラーとルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'posts/index'
   root to: "posts#index"
   resources :users, only: :show
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   resources :posts do
     resources :comments, only: :create
   end
