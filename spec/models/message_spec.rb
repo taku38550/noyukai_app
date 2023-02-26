7578d90c80c9a699225cace2594943da592d60f4
taku38550 <taku_s.1028@icloud.com>
2023-02-26 12:29:07 +0900

    Messageモデルのテスト実行

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
- 
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿できる場合' do
+      it 'contentが存在していれば保存できる' do
+        expect(@message).to be_valid
+      end
+    end
+    
+    context 'メッセージが投稿できない場合' do
+      it 'contentが空では保存できない' do
+        @message.content = ''
+        @message.valid?
+        expect(@message.errors.full_messages).to include()
+      end
+      it 'roomが紐付いていないと保存できない' do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include()
+      end
+      it 'userが紐付いていないと保存できない' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include()
+      end
+    end
+  end
 end
